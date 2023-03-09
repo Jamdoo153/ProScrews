bad50e0313cb408bf65ff8a64e584d21557a3cd4
Michael Walke <Jamdoo@gmx.de>
2023-03-09 08:16:42 +0100

    add Projekt

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Pro_Screw_Market
+{
+    partial class LoadingScreen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingScreen));
+            this.LoadingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.lblLoadingText = new System.Windows.Forms.Label();
+            this.lblLoadingProcess = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadingBarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadingProgressBar
+            // 
+            this.LoadingProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadingProgressBar.Location = new System.Drawing.Point(27, 587);
+            this.LoadingProgressBar.Name = "LoadingProgressBar";
+            this.LoadingProgressBar.Size = new System.Drawing.Size(1144, 30);
+            this.LoadingProgressBar.TabIndex = 0;
+            // 
+            // lblLoadingText
+            // 
+            this.lblLoadingText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLoadingText.AutoSize = true;
+            this.lblLoadingText.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblLoadingText.Location = new System.Drawing.Point(551, 564);
+            this.lblLoadingText.Name = "lblLoadingText";
+            this.lblLoadingText.Size = new System.Drawing.Size(66, 20);
+            this.lblLoadingText.TabIndex = 1;
+            this.lblLoadingText.Text = "Loading";
+            // 
+            // lblLoadingProcess
+            // 
+            this.lblLoadingProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblLoadingProcess.AutoSize = true;
+            this.lblLoadingProcess.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblLoadingProcess.Location = new System.Drawing.Point(623, 564);
+            this.lblLoadingProcess.Name = "lblLoadingProcess";
+            this.lblLoadingProcess.Size = new System.Drawing.Size(32, 20);
+            this.lblLoadingProcess.TabIndex = 2;
+            this.lblLoadingProcess.Text = "0%";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1144, 522);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadingBarTimer
+            // 
+            this.LoadingBarTimer.Interval = 50;
+            this.LoadingBarTimer.Tick += new System.EventHandler(this.LoadingBarTimer_Tick);
+            // 
+            // LoadingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(1196, 629);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblLoadingProcess);
+            this.Controls.Add(this.lblLoadingText);
+            this.Controls.Add(this.LoadingProgressBar);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingScreen";
+            this.Text = "Pro Screw Market";
+            this.Load += new System.EventHandler(this.LoadingScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar LoadingProgressBar;
+        private System.Windows.Forms.Label lblLoadingText;
+        private System.Windows.Forms.Label lblLoadingProcess;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer LoadingBarTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pro_Screw_Market
+{
+    public partial class LoadingScreen : Form
+    {
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private int loadingbatValue;
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            LoadingBarTimer.Start();
+        }
+        
+        private void LoadingBarTimer_Tick(object sender, EventArgs e)
+        {
+
+            loadingbatValue += 1;
+
+            lblLoadingProcess.Text = loadingbatValue.ToString() + "%Mischa";
+
+            LoadingProgressBar.Value = loadingbatValue;
+
+           if(loadingbatValue >= LoadingProgressBar.Maximum)
+            {
+                LoadingBarTimer.Stop();  //Stop Timer bei Value = 100
+            }
+        }
+
+    }
+}
+ 
