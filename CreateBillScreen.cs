e88fd4670da191747acc4e7274ce1ad8e21e93ba
Michael Walke <Jamdoo@gmx.de>
2023-03-11 13:52:22 +0100

    Zweiten Teil Rechnung Erstellung gegonen

---
+++ CreateBillScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pro_Screw_Market
+{
+    public partial class CreateBillScreen : Form
+    {
+        public CreateBillScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateBill_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hallo");
+        }
+    }
+}
